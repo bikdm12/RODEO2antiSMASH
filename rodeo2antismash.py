4a80112dab591c311ff7e95c904c8cc901814ca8
bikdm12 <bikdm12@gmail.com>
2020-03-10 19:14:07 +0300

    no file exception handling

--- rodeo2antismash.py
+++ rodeo2antismash.py
@@ -135,27 +135,30 @@ def convert_gbk(gb_dir, gb_out_dir, rodeo_output, bg_domains, n, product_class):
     
     contig_edge = False
     prot_id = rodeo_output.query
-        
-    genbank = SeqIO.parse('%s%s.gbk' % (gb_dir, prot_id), 'genbank')
-    for record in genbank: # Every file is expected to contain only one record
-        
-        cluster_coords = OrderedDict([('start', 1), ('end', len(record))])
-        
-        for feature in record.features:
-            if feature.type == 'CDS':
-                
-                border_check = check_if_border(feature, operon_border_accs)
-                if border_check is not None:
-                    cluster_coords[border_check[0]] = border_check[1]
+    try:    
+        genbank = SeqIO.parse('%s%s.gbk' % (gb_dir, prot_id), 'genbank')
+        for record in genbank: # Every file is expected to contain only one record
+            
+            cluster_coords = OrderedDict([('start', 1), ('end', len(record))])
+            
+            for feature in record.features:
+                if feature.type == 'CDS':
+                    
+                    border_check = check_if_border(feature, operon_border_accs)
+                    if border_check is not None:
+                        cluster_coords[border_check[0]] = border_check[1]
 
-                if 'protein_id' in feature.qualifiers:
-                    if feature.qualifiers['protein_id'][0] in biosynthetic_genes:
-                        feature.qualifiers['sec_met'] = ['Kind: biosynthetic']
-        
-        start, end = cluster_coords.values()
-        cluster_location = FeatureLocation(start, end)
-        cluster_qualifiers = OrderedDict([('contig_edge', str(contig_edge)), ('product', product_class)])
-        cluster = SeqFeature(location = cluster_location, type = 'cluster', qualifiers = cluster_qualifiers)
-        record.features = [cluster] + record.features
-        
-        SeqIO.write(record, '%s%s.gbk' % (gb_out_dir, prot_id), 'genbank')
+                    if 'protein_id' in feature.qualifiers:
+                        if feature.qualifiers['protein_id'][0] in biosynthetic_genes:
+                            feature.qualifiers['sec_met'] = ['Kind: biosynthetic']
+            
+            start, end = cluster_coords.values()
+            cluster_location = FeatureLocation(start, end)
+            cluster_qualifiers = OrderedDict([('contig_edge', str(contig_edge)), ('product', product_class)])
+            cluster = SeqFeature(location = cluster_location, type = 'cluster', qualifiers = cluster_qualifiers)
+            record.features = [cluster] + record.features
+            
+            SeqIO.write(record, '%s%s.gbk' % (gb_out_dir, prot_id), 'genbank')
+    
+    except Exception as e:
+        print e
